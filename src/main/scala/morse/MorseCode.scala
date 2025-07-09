15e9465ebfdc813f4479ee76a38be2efdd46a3c0
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2025-07-09 23:05:46 +0100

    stored conversion values in MorseCode

--- src/main/scala/morse/MorseCode.scala
+++ src/main/scala/morse/MorseCode.scala
@@ -1,5 +1,19 @@
 package morse
 
 object MorseCode {
+  object MorseCode {
+    val englishToMorse: Map[Char, String] = Map(
+      'A' -> ".-", 'B' -> "-...", 'C' -> "-.-.", 'D' -> "-..",
+      'E' -> ".", 'F' -> "..-.", 'G' -> "--.", 'H' -> "....",
+      'I' -> "..", 'J' -> ".---", 'K' -> "-.-", 'L' -> ".-..",
+      'M' -> "--", 'N' -> "-.", 'O' -> "---", 'P' -> ".--.",
+      'Q' -> "--.-", 'R' -> ".-.", 'S' -> "...", 'T' -> "-",
+      'U' -> "..-", 'V' -> "...-", 'W' -> ".--", 'X' -> "-..-",
+      'Y' -> "-.--", 'Z' -> "--..", ' ' -> "/"
+    )
+    val morseToEnglish: Map[String, Char] =
+      englishToMorse.map { case (k, v) => v -> k }
+  }
+
 
 }
