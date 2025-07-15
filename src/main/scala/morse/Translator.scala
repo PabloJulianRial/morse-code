d0bdcbe53801211239edc89c120a167cf119f836
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2025-07-15 17:16:04 +0100

    add reverse morse code MAP, and removed the Option wrapper from the return value of the translators

--- src/main/scala/morse/Translator.scala
+++ src/main/scala/morse/Translator.scala
@@ -7,14 +7,14 @@ object Translator extends App {
   import morse.MorseCode.englishToMorse
 
   val englishString:String = "HELLO HOW ARE YOU"
-  val morseString:String = ".... . .-.. .-.. --- / .... --- .-- / .- .-. . / -.-- --- ..- "
+  val morseString:String = ".... . .-.. .-.. --- / .... --- .-- / .- .-. . / -.-- --- ..-"
 
   def translatorEtoM(str:String):String = {
     def translatorHelper(acc:String, strLength: Int):String ={
       if (strLength >= str.length) acc
       else translatorHelper(acc + englishToMorse(str.charAt(strLength))  + " ", strLength + 1 )
     }
-    translatorHelper("", 0)
+    translatorHelper("", 0).trim
   }
   println(translatorEtoM(englishString))
 
@@ -26,7 +26,7 @@ object Translator extends App {
       else translatorHelper(acc + morseToEnglish(splitStr(strLength)) , strLength + 1)
     }
 
-    translatorHelper("", 0)
+    translatorHelper("", 0).trim
   }
   println(translatorMtoE(morseString))
 }
--- src/test/scala/morse/TranslatorSpec.scala
+++ src/test/scala/morse/TranslatorSpec.scala
@@ -5,12 +5,12 @@ import org.scalatest.funsuite.AnyFunSuite
 class TranslatorSpec extends AnyFunSuite {
 
   test("translatorEtoM should turn HELLO into the correct Morse code") {
-    val input  = "HELLO"
-    val expect = ".... . .-.. .-.. ---"
+    val input  = "HELLO HOW ARE YOU"
+    val expect = ".... . .-.. .-.. --- / .... --- .-- / .- .-. . / -.-- --- ..-"
     assert(Translator.translatorEtoM(input) == expect)
   }
 
-  test("translatorEtoM on empty string yields empty") {
+  test("translatorEtoM on empty string returns empty") {
     assert(Translator.translatorEtoM("") == "")
   }
 
