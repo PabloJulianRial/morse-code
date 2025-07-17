3522002ce7f12649e816ae550ec19cf984f13341
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2025-07-17 20:56:48 +0100

    refactored input logic from Main to ImputHandler, added recursive function to keep asking for a valid input

--- src/main/scala/morse/InputHandler.scala
+++ src/main/scala/morse/InputHandler.scala
@@ -1,14 +1,29 @@
 package morse
+
 import scala.io.StdIn._
+import morse.MorseCode._
 
-object InputHandler{
+object InputHandler {
+  class InvalidInputException(msg: String) extends Exception(msg)
 
-
-  def readStringInput(): String = {
-     readLine()
-  }
+  def readStringInput(): String =
+    readLine()
   def readIntInput(): Int =
-     readLine().toInt
+    readLine().toInt
+  def wrongStringInput(): String = {
+    println("Sorry, that is neither a valid Morse Code String or an English String")
+    readStringInput().toUpperCase
+  }
+  def checkValidInput(inputString: String): Boolean = inputString match {
+    case s if s.forall(ch => englishToMorse.contains(ch)) => true
+    case s if s.split("\\s+").forall(tok => morseToEnglish.contains(tok)) => true
+    case _ => false
+  }
+  def readUntilValid(initial: String): String = {
+    if (checkValidInput(initial)) initial
+    else {
+      val retry = wrongStringInput()
+      readUntilValid(retry)
+    }
+  }
 }
-
-class InvalidInputException(e: String) extends Exception(e)
--- src/main/scala/morse/Main.scala
+++ src/main/scala/morse/Main.scala
@@ -1,8 +1,9 @@
 package morse
+
 import morse.InputHandler._
 import morse.OutPutHandler._
 import morse.Translator._
-import morse.MorseCode._
+
 object Main extends App {
 
   printMenu()
@@ -12,31 +13,13 @@ object Main extends App {
   val inputPrompt = choice match {
     case 1 => "Enter Morse code: "
     case 2 => "Enter English text: "
-    case _ =>
-      throw new InvalidInputException(s"Invalid choice ($choice), select 1 or 2")
-  }
-  def wrongStringInput():String = {
-    println("Sorry, that is neither a valid Morse Code String or an English String")
-    readStringInput().toUpperCase
+    case _ => throw new InvalidInputException(s"Invalid choice ($choice), select 1 or 2")
   }
 
   promptInput(inputPrompt)
-  val input = readStringInput().toUpperCase
-  
-  def checkValidInput(inputString: String):Boolean = {
-  inputString match {
-    case inputString if inputString.forall(ch => englishToMorse.contains(ch)) => true
-    case inputString if inputString.split("\\s+").forall(ch => morseToEnglish.contains(ch)) => true
-    case _ => false
-     }
-  }
-  
-  if(!checkValidInput(input)) {
-    println("Sorry, that is neither valid English nor valid Morse.")
-    promptInput(inputPrompt)
-    val input = readStringInput().toUpperCase
-  }
-  //val unwrappedInput:String = input.getOrElse(wrongStringInput())
+  val firstInput = readStringInput().toUpperCase
+
+  val input = readUntilValid(firstInput)
 
   val result = choice match {
     case 1 => translatorMtoE(input)
@@ -44,6 +27,4 @@ object Main extends App {
   }
 
   printResult(result)
-
-
 }
